b99c6324f6c5c7630517868b3fe2b42f27cbff41
EdoardoDeDivitiis <81361607+EdoardoDeDivitiis@users.noreply.github.com>
2023-12-05 22:00:57 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,1 @@
+const express=require("express");const path=require("path");const app=express();const port=3333;app.use(express.static(path.join(__dirname,"public")));app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"public","index.html"))});app.listen(port,()=>{console.log(`Server listening at http://localhost:${port}`)});
